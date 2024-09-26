62d4a0e819f221e8b4df3fdc590866d245b9f284
Illya Manin <ilay.manin@gmail.com>
2024-09-26 00:02:14 -0700

    added images being cropped to hardcoded % values + added Nagisa for testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 import Image_display from './ImageDisplay.js';
-import hoshino_battle from './images/Hoshino_(Battle)_00.png';
+import hoshino_battle from './images/Hoshino_Battle_00.png';
 import mika from './images/Mika_00.png'
+import nagisa from './images/Nagisa_00.png'
 
 function App() {
   const [image, setImage] = useState('');
@@ -11,6 +12,7 @@ function App() {
 
       <button onClick={() => setImage(hoshino_battle)}>See Hoshino</button>
       <button onClick={() => setImage(mika)}>See Mika</button>
+      <button onClick={() => setImage(nagisa)}>See Nagisa</button>
       {<Image_display image_url={image}/>}
     </div>
   );
--- src/ImageDisplay.js
+++ src/ImageDisplay.js
@@ -1,8 +1,17 @@
-import hoshino_battle from './images/Hoshino_(Battle)_00.png';
-import mika from './images/Mika_00.png'
+import hoshino_battle from './images/Hoshino_Battle_00.png';
+import mika from './images/Mika_00.png';
+import {View, Image} from 'react-native';
 
 export default function Image_display({image_url}) {
     return (
-        <img src={image_url}></img>
+    <div style={{
+        width:"500px", 
+        height:"500px", 
+        overflow:'hidden', 
+        backgroundImage:`url(${image_url})`,
+        backgroundPosition: '20% 40%'
+        }}>
+    </div>
+
     )
 }
